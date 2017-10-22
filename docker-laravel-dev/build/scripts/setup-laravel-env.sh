d416b1a363fe7a27cc57c5f113e9a2e2e8d6ded9
Pablo G <dev@pega.sh>
2017-10-22 20:56:25 -0300

    - Converts Dockerfile scripting to external scripts

---
+++ docker-laravel-dev/build/scripts/setup-laravel-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Cloning latest laravel source from official repo
+rm -rf /var/www/html
+git clone https://github.com/laravel/laravel /var/www/html
+
+# Composer needs full access to the repository and doesn't want to be run as root.
+
+cd /var/www/html
+chown www-data:www-data . -R
+mv .env.example .env
+su www-data -s /bin/bash -c "composer install"
+su www-data -s /bin/bash -c "php artisan key:generate"
+
+# Allows writing only on public directories
+
+chown root:root . -R
+chown www-data:www-data storage bootstrap/cache -R
