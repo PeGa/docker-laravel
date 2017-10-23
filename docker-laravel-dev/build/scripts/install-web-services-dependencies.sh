68c57aeb740cd44f2ec231e0d5e41b59476d5e63
PeGa! <dev@pega.sh>
2017-10-23 20:00:02 -0300

    Fixes unpredicted location of composer.phar

--- docker-laravel-dev/build/scripts/install-web-services-dependencies.sh
+++ docker-laravel-dev/build/scripts/install-web-services-dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# cd to /tmp so we can grab composer.phar at a predictable location
+cd /tmp/
+
 phpPackages="
 nginx-extras
 php7.0-fpm
@@ -10,7 +14,7 @@ php7.0-mysql
 "
 apt install -y $phpPackages
 
-wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php # -- --quiet
+wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | php  -- --quiet
 
 mv /tmp/composer.phar /usr/local/bin/composer
 
