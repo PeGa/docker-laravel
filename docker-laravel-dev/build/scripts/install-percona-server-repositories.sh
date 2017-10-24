85d6915e57e95a3ef8f96b25704e4d860554371b
PeGa! <dev@pega.sh>
2017-10-23 21:59:29 -0300

    Adds lsb-release for percona-server repositories installation

--- docker-laravel-dev/build/scripts/install-percona-server-repositories.sh
+++ docker-laravel-dev/build/scripts/install-percona-server-repositories.sh
@@ -5,6 +5,7 @@
 # first of all in the whole setup process.
 
 apt update
-apt install git wget -y
+apt install git wget lsb-release -y
+cd /tmp/
 wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
 dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
